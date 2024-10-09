bca25d8e718f5460307ebf8c3e000e28bfe9097a
Ajay Kumar <ajaykumargogineni@gmail.com>
2024-10-09 22:18:34 +0530

    added employee model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeModel } from './model/Employee';
+import { createInjectableType } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,24 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular_18_crud';
+
+  employeeForm? : FormGroup;
+  employeeobj: EmployeeModel = new EmployeeModel();
+
+  constructor(){
+    this.createFrom();
+  }
+
+  createForm(){
+    this.employeeForm = new FormGroup({
+      empid : new FormControl(this.employeeobj.emailId),
+      name:new FormControl(this.employeeobj.name),
+      city:new FormControl(this.employeeobj.city),
+      address:new FormControl(this.employeeobj.address),
+      contactNo:new FormControl(this.employeeobj.contactNo),
+      emailId:new FormControl(this.employeeobj.emailId),
+      pinCode:new FormControl(this.employeeobj.pinCode),
+      state:new FormControl(this.employeeobj.state)
+    });
+  }
 }
